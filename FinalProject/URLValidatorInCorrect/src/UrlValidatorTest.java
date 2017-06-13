e1067760a97557a4497d8ab920cc9ca83febe9c9
Penelope Mabry <mabryp@flip2.engr.oregonstate.edu>
2017-06-12 21:21:42 -0700

    Added updated test file for final project

--- FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
+++ FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
@@ -27,13 +27,22 @@ import junit.framework.TestCase;
  *
  * @version $Revision: 1128446 $ $Date: 2011-05-27 13:29:27 -0700 (Fri, 27 May 2011) $
  */
-public class UrlValidatorTest extends TestCase {
 
-   private boolean printStatus = false;
-   private boolean printIndex = false;//print index that indicates current scheme,host,port,path, query test were using.
+
+public class UrlValidatorTest extends TestCase {
 
    public UrlValidatorTest(String testName) {
       super(testName);
+   }
+   
+
+   public void iterateOverTestParts(Object [] testParts, int testPartIndex[])
+   {
+	   int i = 0;
+	   int j = 0; 
+	   int k = 0;
+	   
+	   
    }
 
    
@@ -41,40 +50,174 @@ public class UrlValidatorTest extends TestCase {
    public void testManualTest()
    {
 	   UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
-	   System.out.println(urlVal.isValid("http://www.amazon.com"));
+	   
+	   //System.out.println(urlVal.isValid("http://www.amazon.com"));
+
+	   assertTrue(urlVal.isValid("http://www.amazon.com"));
+	   assertTrue(urlVal.isValid("http://www.amazon.com/"));
+	   assertTrue(urlVal.isValid("http://www.amazon.com/images"));
+	   assertTrue(urlVal.isValid("http://www.amazon.com/images/"));
+	   assertTrue(urlVal.isValid("http://www.amazon.com/images/funnyCat.png"));
+	   assertTrue(urlVal.isValid("http://www.amazon.com/images/funnyCat(1).png"));
+	   assertTrue(urlVal.isValid("http://www.amazon.com/images/funnyCat_1.png"));
+	   assertTrue(urlVal.isValid("http://www.amazon.com/images/funnyCat-1.png"));
+	   assertTrue(urlVal.isValid("http://www.amazon.com/images/funnyCat.png.jpeg"));
+	   assertTrue(urlVal.isValid("http://www.amazon.com/images/funnyCat(/).png.jpeg"));
+
+
+	   assertTrue(urlVal.isValid("http://www.amazon.com:80"));
+	   assertTrue(urlVal.isValid("http://amazon.com:80"));
+	   
+	   assertTrue(urlVal.isValid("http://amazon.com"));
+	   assertTrue(urlVal.isValid("http://amazon.com/"));
+	   assertTrue(urlVal.isValid("http://amazon.com/images"));
+	   assertTrue(urlVal.isValid("http://amazon.com/images/funnyCat.png"));
+	   assertTrue(urlVal.isValid("http://amazon.com/images/funnyCat(1).png"));
+	   assertTrue(urlVal.isValid("http://amazon.com/images/funnyCat_1.png"));
+	   assertTrue(urlVal.isValid("http://amazon.com/images/funnyCat-1.png"));
+	   assertTrue(urlVal.isValid("http://amazon.com/images/funnyCat.png.jpeg"));
+	   assertTrue(urlVal.isValid("http://amazon.com/images/funny.Cat.png.jpeg"));
+
+	   assertTrue(urlVal.isValid("http://www.amazon.com:0"));
+	   assertTrue(urlVal.isValid("http://amazon.com:0"));
 	   
 	   
-   }
-   
-   
-   public void testYourFirstPartition()
-   {
+	   assertFalse(urlVal.isValid("http:/www.amazon.com//"));
+	   assertFalse(urlVal.isValid("http:/www.amazon.com//.."));
+	   assertFalse(urlVal.isValid("http:/www.amazon.com/./"));
+	   assertFalse(urlVal.isValid("http:/www.amazon.com/./"));
+	   assertFalse(urlVal.isValid("http:/www.amazon.com////"));
+
 	   
-   }
-   
-   public void testYourSecondPartition(){
+	   assertFalse(urlVal.isValid("http://##"));
+
+	   
+	   
+	   assertFalse(urlVal.isValid("http:/amazon.com"));
+	   assertFalse(urlVal.isValid("http//amazon.com"));
+	   assertFalse(urlVal.isValid("http///amazon.com"));
+	   assertFalse(urlVal.isValid("hp:/amazon.com"));
+	   assertFalse(urlVal.isValid("amazon.com"));
+
+
+	   assertFalse(urlVal.isValid("http:/www.amazon.com"));
+	   assertFalse(urlVal.isValid("http//www.amazon.com"));
+	   assertFalse(urlVal.isValid("http///www.amazon.com"));
+	   assertFalse(urlVal.isValid("hp:/www.amazon.com"));
+
+	   
+	   
+	   assertFalse(urlVal.isValid("http://www.amazon.com:"));
+	   assertFalse(urlVal.isValid("http://www..amazon.com:"));
+	   assertFalse(urlVal.isValid("http://www..amazon,com:"));
+	   assertFalse(urlVal.isValid("http://amazon.com:"));
+	   
+	   
+
+	   
+	   assertFalse(urlVal.isValid("http://www.amazon.com:-1"));
+	   assertFalse(urlVal.isValid("http://amazon.com:-1"));
+	   
+	   assertFalse(urlVal.isValid("http://www.amazon.com:65534")); 	//invalid URL
+	   assertFalse(urlVal.isValid("http://www.amazon.com:70000")); 	//invalid URL
+	   assertFalse(urlVal.isValid("http://www.amazon.com:700000"));	//invalid URL
+	   assertFalse(urlVal.isValid("http://www.amazon..com:65535")); //invalid URL
+	  
+	   assertFalse(urlVal.isValid("http://amazon.com:65534")); 		//invalid URL
+	   assertFalse(urlVal.isValid("http://amazon.com:70000")); 		//invalid URL
+	   assertFalse(urlVal.isValid("http://amazon.com:700000"));		//invalid URL
+	   assertFalse(urlVal.isValid("http://amazon..com:65535")); 	//invalid URL
+
+	   assertFalse(urlVal.isValid("http://amazon.com:1000"));
+	   
+	   assertFalse(urlVal.isValid("http:// "));
+	   assertFalse(urlVal.isValid("http:// .com"));
+	   assertFalse(urlVal.isValid("http://  .com"));
+	   assertFalse(urlVal.isValid("http://www. .com"));
+	   assertFalse(urlVal.isValid("http://www...com"));
+	   
+	   assertFalse(urlVal.isValid("http://www.amazon.com/images/funnyCat(//).png.jpeg"));
+
+	   System.out.println("---PORT NUMBER TESTING---");
+	   	
+	   for(int i = 0; i < 1000; i++)
+	   {
+		   assertTrue(urlVal.isValid("http://amazon.com:" + String.valueOf(i)));
+	   }
+	   
+	   System.out.println(" 'http://amazon.com:' + String.valueOf(i) from i = 0 thru i = 999 all returned valid URLs correctly");
+
+	   
+	   for(int i = 1000; i < 65536; i++)
+	   {
+		   
+		   assertFalse(urlVal.isValid("http://amazon.com:" + String.valueOf(i)));
+	   }
+	   
+	   System.out.println(" 'http://amazon.com:' + String.valueOf(i) from i = 1000 thru i = 65535 all returned invalid URLs incorrectly ");
+
+	   
+	   System.out.println("---PORT NUMBER TESTING FINISHED---");
+
 	   
    }
    
    
    public void testIsValid()
    {
-	   for(int i = 0;i<10000;i++)
-	   {
+	   System.out.println("Testing isValid function of URL validator: ");
+	   System.out.println("Testing validURLs: ");
+	   testArray(validURLs, true);
+	   
+	   System.out.println("Testing invalidURLs: ");
+	   testArray(invalidURLs, false);
+	   
+	   System.out.println("Testing Complete");
+   }
+   
+   public void testArray(String[] strArr, boolean isValid){
+	   
+	   UrlValidator validator = new UrlValidator();
+	   
+	   for(int i = 0; i < strArr.length; i++){
 		   
+		   boolean result = validator.isValid(strArr[i]);
+		   
+		   assertEquals(isValid, result);
+		   
+		   if (isValid == result){
+			   System.out.printf("%-20s %-5s %-40s %-10s %-5s\n","TEST PASSED!","URL: ", strArr[i], "Result: ", result);			   
+		   }
 	   }
    }
    
-   public void testAnyOtherUnitTest()
-   {
-	   
-   }
-   /**
-    * Create set of tests by taking the testUrlXXX arrays and
-    * running through all possible permutations of their combinations.
-    *
-    * @param testObjects Used to create a url.
-    */
+   String[] validURLs = {"https://www.google.com/",
+		   			"https://en.wikipedia.org/wiki/URL",
+		   			"http://www.amazon.com",
+		   			"http://255.255.255.255",
+		   			"http://0.0.0.0",
+		   			"https://www.google.com/search?q=puppies",
+		   			"Flip3.engr.oregonstate.edu:5775",
+		   			"Flip3.engr.oregonstate.edu:0",
+		   			"Flip3.engr.oregonstate.edu:57",
+		   			"Flip3.engr.oregonstate.edu:579",
+		   			"http://www.amazon.com:65536"	
+		   					   
+   };
+   String[] invalidURLs = {"httpps://www.google.com/",
+		   			"https:/www.google.com",
+		   			"https://www.google.c",
+		   			"http:/www.google.com",
+		   			"http:://www.google.com",
+		   			"Flip3.engr.oregonstate.edu:57755",
+		   			"flip3..oregonstate.edu",
+		   			"Flip3.engr.oregonstate.edu:57755",
+		   			"http://www...com",
+		   			"http:/www.google.c00",
+		   			"://www.google.com",
+		   			"http://256.256.256.256",
+		   			"http://999.999.999.999"
+		   
+   };
    
-
 }
